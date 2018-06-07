7061d4c3c4ad01cf351d656e4eda19acea45c76b
Palameng <506357902@qq.com>
2018-06-07 20:46:05 +0800

    2018/6/7 完成商品类别管理的基本操作功能

--- src/main/java/com/my/o2o/service/impl/ProductCategoryServiceImpl.java
+++ src/main/java/com/my/o2o/service/impl/ProductCategoryServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.my.o2o.dao.ProductCategoryDao;
 import com.my.o2o.dto.ProductCategoryExecution;
@@ -43,4 +44,21 @@ public class ProductCategoryServiceImpl implements ProductCategoryService{
         }
     }
 
+    @Override
+    @Transactional
+    public ProductCategoryExecution deleteProductCategory(long productCategoryId, long shopId)
+            throws ProductCategoryOperationException {
+        // TODO 将此类别下的商品里的类别ID置为空
+        
+        try {
+            int effectedNum = productCategoryDao.deleteProductCategory(productCategoryId, shopId);
+            if (effectedNum <= 0) {
+                throw new ProductCategoryOperationException("商品类别删除失败");
+            }else {
+                return new ProductCategoryExecution(ProductCategoryStateEnum.SUCCESS);
+            }
+        } catch (Exception e) {
+            throw new ProductCategoryOperationException("deleteProductCategory error:" + e.getMessage());
+        }
+    }
 }
--- src/test/java/com/my/o2o/dao/ProductCategoryDaoTest.java
+++ src/test/java/com/my/o2o/dao/ProductCategoryDaoTest.java
@@ -4,28 +4,37 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.junit.FixMethodOrder;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.my.o2o.BaseTest;
 import com.my.o2o.entity.ProductCategory;
 import static org.junit.Assert.assertEquals;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ProductCategoryDaoTest extends BaseTest{
     @Autowired
     private ProductCategoryDao productCategoryDao;
     
+    /**
+     * 搜索商品类别列表
+     * @throws Exception
+     */
     @Test
-    @Ignore
-    public void testQueryByShopId() throws Exception {
+    public void testA_QueryByShopId() throws Exception {
         long shopId = 1L;
         List<ProductCategory> productCategories = productCategoryDao.queryProductCategoryList(shopId);
         System.out.println("该店铺自定义类别数为:" + productCategories.size());
     }
     
+    /**
+     * 批量添加商品列表
+     */
     @Test
-    public void testBatchInsertProductCategory(){
+    public void testB_BatchInsertProductCategory(){
         ProductCategory productCategory = new ProductCategory();
         productCategory.setProductCategoryName("烘焙类");
         productCategory.setPriority(1);
@@ -44,5 +53,22 @@ public class ProductCategoryDaoTest extends BaseTest{
         
         int effectedNum = productCategoryDao.batchInsertProductCategory(productCategoryList);
         assertEquals(2, effectedNum);
+    }
+    
+    /**
+     * UT回环，即上一个方法测试了批量增加，这里做删除操作
+     * @throws Exception
+     */
+    @Test
+    public void testC_DeleteProductCategory() throws Exception{
+        long shopId = 1;
+        List<ProductCategory> productCategories = productCategoryDao.queryProductCategoryList(shopId);
+        
+        for(ProductCategory pc : productCategories){
+            if ("烘焙类".equals(pc.getProductCategoryName()) || "面食类".equals(pc.getProductCategoryName())) {
+                int effectedNum = productCategoryDao.deleteProductCategory(pc.getProductCategoryId(), shopId);
+                assertEquals(1, effectedNum);
+            }
+        }
     }
 }
